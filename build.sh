87f9dc78005712edcace24abf7c14d2462e19b23
Rado <spacest@gmail.com>
2022-03-16 19:39:47 +0000

    ,

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ./node_modules/sass/sass.js text-media.scss text-media.css
 ./node_modules/sass/sass.js demo/demo.scss demo/demo.css
 ./node_modules/clean-css-cli/bin/cleancss -o text-media.min.css text-media.css
-gzip-size --raw text-media.min.css > text-media.min.css.size
 ./node_modules/terser/bin/terser -o text-media.min.js --compress --mangle -- text-media.js
-gzip-size --raw text-media.min.js > text-media.min.js.size
+./node_modules/gzip-size-cli/cli.js --raw text-media.min.css > text-media.min.css.size
+./node_modules/gzip-size-cli/cli.js --raw text-media.min.js > text-media.min.js.size
